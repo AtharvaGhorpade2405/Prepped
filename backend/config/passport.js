bc2bed9d5d3cb57c3e79759a2fd1821d941fbbec
AtharvaGhorpade2405 <atharvasg24@gmail.com>
2026-01-11 00:00:43 +0530

    fixed session error

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -10,7 +10,6 @@ export default function configurePassport() {
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         callbackURL: "https://prepped-backend-8fgp.onrender.com/api/auth/google/callback",
         // callbackURL: "http://localhost:5000/api/auth/google/callback",
-        state:false
       },
       async (accessToken, refreshToken, profile, done) => {
         try {
